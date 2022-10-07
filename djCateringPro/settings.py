e44c48bc1026fd5f55f51a89f91bd7ecd8bff794
pcb-github <pcb.aar@gmail.com>
2022-10-07 08:47:11 +0300

    changes

--- djCateringPro/settings.py
+++ djCateringPro/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
